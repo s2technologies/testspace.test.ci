165602661fa6a1ed5e338129a2587c11c7cc502b
Krum Motsov <kmotsov@gmail.com>
2018-07-30 17:37:45 +0300

    typo

--- ts-time-push.sh
+++ ts-time-push.sh
@@ -12,7 +12,7 @@ NUM_FILES=$(ls -l result*.xml | grep -v ^d | wc -l )
 echo "TIMING STATS:" > timing.log
 echo "  Number of files: $NUM_FILES" >> timing.log
 echo "  Size of content(files X 3): $CONTENT_SIZE" >> timing.log
-echo "  $JOB Upload time in milliseconds, $TIME_DIFF" >> timing.log
+echo "  $JOB Upload time in milliseconds: $TIME_DIFF" >> timing.log
 
 # CSV file
 echo "$JOB Upload time, $TIME_DIFF" > timing.csv
