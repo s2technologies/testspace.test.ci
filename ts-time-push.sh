ec061f078b9f937fe3e019f17b819e553c6230c7
Krum Motsov <kmotsov@gmail.com>
2018-07-30 17:11:21 +0300

    Calculate elapsed time in ms

--- ts-time-push.sh
+++ ts-time-push.sh
@@ -1,6 +1,6 @@
-TIME_START=$(date +%s)
+TIME_START=$(date +%s%3N)
 testspace result*.xml [ALL]result*.xml [$JOB]result*.xml
-TIME_END=$(date +%s)
+TIME_END=$(date +%s%3N)
 TIME_DIFF=$(( $TIME_END - $TIME_START ))
 
 testspace results.xml [ALL]results.xml [$JOB]results.xml file://$PWD/check-size.xml
@@ -12,7 +12,7 @@ NUM_FILES=$(ls -l result*.xml | grep -v ^d | wc -l )
 echo "TIMING STATS:" > timing.log
 echo "  Number of files: $NUM_FILES" >> timing.log
 echo "  Size of content(files X 3): $CONTENT_SIZE" >> timing.log
-echo "  $JOB Upload time in seconds, $TIME_DIFF" >> timing.log
+echo "  $JOB Upload time in milliseconds, $TIME_DIFF" >> timing.log
 
 # CSV file
 echo "$JOB Upload time, $TIME_DIFF" > timing.csv
