5313208dab6304d30f31302b523817cb23d08aa6
Ivailo Petrov <ivailop@s2technologies.com>
2018-08-31 12:20:37 -0700

    added appveyor timing

--- ts-time-push.bat
+++ ts-time-push.bat
@@ -7,7 +7,7 @@ call:time2ms TIME_END
 set /a TIME_DIFF=%TIME_END% - %TIME_START%
 
 testspace results.xml [ALL]results.xml [%JOB%]results.xml file://%CD%/check-size.xml
-for /f "tokens=*" %%A IN ("check-size.xml") do set CONTENT_SIZE=%%~zA
+for /f "tokens=*" %%A in ("check-size.xml") do set CONTENT_SIZE=%%~zA
 
 for /f %%A in ('dir result*.xml ^| find "File(s)"') do set NUM_FILES=%%A
 
@@ -30,6 +30,5 @@ goto:eof
 setlocal
 rem The format of %TIME% is HH:MM:SS,CS for example 23:59:59,99 (centiseconds)
 set t=%TIME%
-echo %t%
 endlocal&set /a %~1=(1%t:~0,2%-100)*3600000 + (1%t:~3,2%-100)*60000 + (1%t:~6,2%-100)*1000 + (1%t:~9,2%-100)*10
 goto:eof
