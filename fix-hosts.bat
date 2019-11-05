f2331c760e38b2ae31d08f47521d611c38124e92
Ivailo Petrov <ivailop@s2technologies.com>
2019-11-05 15:04:20 -0800

    do 'hosts' fixing in a script

---
+++ fix-hosts.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+
+set /p IP_URL=<commit-trigger.txt
+for /f "tokens=1" %%a IN ("%IP_URL%") DO (set IP=%%a)
+for /f "tokens=2" %%a IN ("%IP_URL%") DO (set URL=%%a)
+set DOMAIN=%URL:http://=%
+if "%DOMAIN%" == "s2.test.stridespace.com" (
+  echo. >> "%windir%\System32\drivers\etc\hosts"
+  echo %IP% %DOMAIN%>>"%windir%\System32\drivers\etc\hosts"
+)
+
+echo %URL%> testspace-url
