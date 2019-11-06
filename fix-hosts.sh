8021c6bd38f5a938a81ec71b401d618ca2d65fe8
Ivailo Petrov <ivailop@s2technologies.com>
2019-11-06 12:01:07 -0800

    do 'hosts' fixing in a script

---
+++ fix-hosts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+IP_URL=$(< commit-trigger.txt)
+IP=${IP_URL% *}
+URL=${IP_URL#* }
+DOMAIN=${URL#http://}
+test "$IP" -a "$DOMAIN"
+if [ "$DOMAIN" = "s2.test.stridespace.com" ]; then
+  echo $IP $DOMAIN | sudo tee -a /etc/hosts
+fi
+
+echo $URL > testspace-url
