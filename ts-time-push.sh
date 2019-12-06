b348948cd07ce51b642977172b957081e2f30fbf
Ivailo Petrov <ivailop@s2technologies.com>
2019-12-06 08:58:33 -0800

    write 'timing' files in TMP dir

--- ts-time-push.sh
+++ ts-time-push.sh
@@ -11,13 +11,13 @@ CONTENT_SIZE=$(stat -c  '%s' check-size.xml)
 NUM_FILES=$(ls -l result*.xml | grep -v ^d | wc -l )
 
 # Log file
-echo "TIMING STATS:" > timing.log
-echo "  Number of files: $NUM_FILES" >> timing.log
-echo "  Size of content(files X 3): $CONTENT_SIZE" >> timing.log
-echo "  $JOB Upload time in milliseconds: $TIME_DIFF" >> timing.log
+echo "TIMING STATS:" > /tmp/timing.log
+echo "  Number of files: $NUM_FILES" >> /tmp/timing.log
+echo "  Size of content(files X 3): $CONTENT_SIZE" >> /tmp/timing.log
+echo "  $JOB Upload time in milliseconds: $TIME_DIFF" >> /tmp/timing.log
 
 # CSV file
-echo "$JOB Upload time, $TIME_DIFF" > timing.csv
-echo "$JOB Content size, $CONTENT_SIZE" >> timing.csv
+echo "$JOB Upload time, $TIME_DIFF" > /tmp/timing.csv
+echo "$JOB Content size, $CONTENT_SIZE" >> /tmp/timing.csv
 
-testspace [$JOB]"timing.log{timing.csv:Tracking Time for $JOB}"
+testspace [$JOB]"/tmp/timing.log{/tmp/timing.csv:Tracking Time for $JOB}"
