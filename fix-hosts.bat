534fa59ff854ad56a3c09e5bf80717e12dcce437
Ivailo Petrov <ivailop@s2technologies.com>
2019-12-06 09:23:03 -0800

    change 'hosts' write condition

--- fix-hosts.bat
+++ fix-hosts.bat
@@ -5,7 +5,7 @@ set /p IP_URL=<commit-trigger.txt
 for /f "tokens=1" %%a IN ("%IP_URL%") DO (set IP=%%a)
 for /f "tokens=2" %%a IN ("%IP_URL%") DO (set URL=%%a)
 set DOMAIN=%URL:http://=%
-if "%DOMAIN%" == "s2.test.stridespace.com" (
+if not "%DOMAIN%" == "s2.stridespace.com" (
   echo. >> "%windir%\System32\drivers\etc\hosts"
   echo %IP% %DOMAIN%>>"%windir%\System32\drivers\etc\hosts"
 )
