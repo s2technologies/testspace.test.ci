01a488e7e6f7bfb400476a1fd7ba419148db9b75
Ivailo Petrov <ivailop@s2technologies.com>
2019-12-20 10:12:58 -0800

    write to 'hosts' instead of dnsmasq

--- fix-hosts.sh
+++ fix-hosts.sh
@@ -7,12 +7,7 @@ URL=${IP_URL#* }
 DOMAIN=${URL#http://}
 test "$IP" -a "$DOMAIN"
 if [ "$DOMAIN" != "s2.stridespace.com" ]; then
-  sudo apt-get install -qq -y --force-yes dnsmasq
-  echo "listen-address=127.0.0.1" | sudo tee -a /etc/dnsmasq.conf > /dev/null 2>&1
-  echo "bind-interfaces" | sudo tee -a /etc/dnsmasq.conf > /dev/null 2>&1
-  echo "address=/$DOMAIN/$IP" | sudo tee -a /etc/dnsmasq.conf
-  echo "user=root" | sudo tee -a /etc/dnsmasq.conf > /dev/null 2>&1
-  sudo service dnsmasq restart
+  echo $IP $DOMAIN | sudo tee -a /etc/hosts
 fi
 
 echo $URL > testspace-url
